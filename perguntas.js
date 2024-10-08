58f784f2e0ba05a978149269b466f15ee1624ee0
Amanda <141869873+LaraAmanda@users.noreply.github.com>
2024-10-08 12:23:26 -0300

    dfjkjkfdjk

---
+++ perguntas.js
@@ -0,0 +1,112 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)).q)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a média aritmética?',
+    'A média aritmética é a soma dos valores dividida pela quantidade de valores'
+)
+
+ criaCartao(
+    'Fisica',
+    'Qual é a 1ª lei de Newton?',
+    '1ª Lei (Inércia): Um corpo em repouso permanece em repouso, e um corpo em movimento permanece em movimento, a menos que uma força externa atue sobre ele'
+ )
+
+ criaCartao(
+    'Química',
+    'O que é a tabela periódica?',
+    'A tabela periódica organiza os elementos químicos de acordo com suas propriedades e número atômico'
+ )
+
+ criaCartao(
+    'Química',
+    'Quais são os principais tipos de ligações químicas?',
+    'Ligação iônica, Ligação covalente e Ligação metálica'
+ )
+
+ criaCartao(
+    'História',
+    'O que foram as principais causas da Revolução Francesa?',
+    'Desigualdade social, crise financeira, influência das ideias iluministas, e a insatisfação com a monarquia absolutista'
+ )
+
+ criaCartao(
+    'História',
+    ' Quem descobriu o Brasil?',
+    ' Pedro Alvares Cabral'
+ )
+
+
+criaCartao(
+    'Geografia',
+    'O que é a teoria da tectônica de placas?',
+    'É a teoria que descreve a movimentação das placas tectônicas da crosta terrestre, explicando fenômenos como terremotos, vulcanismo e a formação de montanhas'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'A fórmula é A=𝜋r^2, onde r é o raio do círculo.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a Lei da Conservação da Energia?',
+    'A energia total em um sistema isolado permanece constante, a energia pode ser transformada de uma forma para outra, mas não pode ser criada nem destruída'
+)
+
+criaCartao(
+    'Química',
+    'O que são isótopos?',
+    'Isótopos são átomos do mesmo elemento com o mesmo número de prótons, mas diferente número de nêutrons'
+)
+
+criaCartao(
+    'Química',
+    'Quais são os principais tipos de reações químicas?',
+    'Reação de síntese, reação de decomposição, reação de deslocamento simples e reação de dupla troca.'
+)
+
+criaCartao(
+    'Química',
+    'O que caracteriza uma solução?',
+    'Uma solução é uma mistura homogênea onde um soluto é dissolvido em um solvente.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a teoria da evolução de Darwin?',
+    'A teoria propõe que as espécies evoluem ao longo do tempo por meio de um processo de seleção natural, onde os indivíduos mais adaptados ao ambiente têm maior chance de sobreviver e reproduzir.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função dos ribossomos?',
+    'Ribossomos são responsáveis pela síntese de proteínas, traduzindo o RNA mensageiro em cadeias polipeptídicas'
+)
+
+criaCartao(
+    'Hístoria',
+    'Quais foram as consequências da Segunda Guerra Mundial?',
+    'Criação da ONU, Guerra Fria, descolonização de várias nações e divisão da Alemanha'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um ecossistema?',
+    'Um ecossistema é uma comunidade de organismos interagindo entre si e com o ambiente físico em que vivem'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a diferença entre clima e tempo?',
+    'Clima refere-se às condições atmosféricas médias de uma região ao longo do tempo, enquanto tempo refere-se às condições atmosféricas em um determinado momento.'
+)
+criaCartao(
+    'Sociologia',
+    'O que é o racionalismo e quem são seus principais representantes?',
+)
