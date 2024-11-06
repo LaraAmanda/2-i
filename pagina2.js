4ac8f5885fcc37aab12376997db1ce017050f47b
Amanda <141869873+LaraAmanda@users.noreply.github.com>
2024-11-06 11:53:23 -0300

    heh

--- pagina2.js
+++ pagina2.js
@@ -31,7 +31,7 @@ container.appendChild(cartao)
 function mostraresposta(card){
     const resposta =card.querySelector(' .resposta-cartao')
     if(resposta.style.display === 'none'){
-        resposta.style.display = 'block'
+        resposta.style.display = 'block';
 
     }
 }
